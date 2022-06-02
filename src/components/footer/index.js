e4adf09ce1536698847caf72e62350853afba2c3
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-02 13:03:30 -0400

    footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+
+    return (
+        <footer>
+            <div className='flex-row mx-2'>
+                <h4>
+                    <a href="https://github.com/athanasiak18" target="_blank" rel="noopener noreferrer" className='mx-2'>
+                        Github
+
+                    </a>
+                </h4>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
