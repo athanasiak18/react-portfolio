dca1b3c29065323dcdeef6deb8c5d1a30065d473
Athanasia <athanasiakotopoulos@gmail.com>
2022-06-02 13:10:08 -0400

    default

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import './App.css';
+import React, { useState } from "react";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import About from './components/About';
+import Nav from './components/Nav';
+import Contact from './components/Contact';
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+export default App;
